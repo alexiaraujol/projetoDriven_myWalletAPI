818016caa0c4effdb06c29df9fa76e688ce80df7
Alexia A. de Lima <alexiaalima96@gmail.com>
2025-05-15 14:24:27 -0300

    fix: Database

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const mongoURL = process.env.MONGO_URL
+const mongoURL = process.env.DATABASE_URL;
 const mongoClient = new MongoClient(mongoURL);
 
 try {
